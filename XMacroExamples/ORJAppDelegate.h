7446cfa793dda5511e93d1420394b860d6452b3a
Oliver Jones <oliver@deeperdesign.com>
2013-03-18 22:37:49 +1100

    Initial Commit

---
+++ XMacroExamples/ORJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ORJAppDelegate.h
+//  XMacroExamples
+//
+//  Created by Oliver Jones on 18/03/13.
+//  Copyright (c) 2013 Oliver Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ORJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XMacroExamples/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XMacroExamples
+//
+//  Created by Oliver Jones on 18/03/13.
+//  Copyright (c) 2013 Oliver Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ORJAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ORJAppDelegate class]));
+    }
+}
